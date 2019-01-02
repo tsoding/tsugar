9cff13b979c98c677367a5e78bc1db2029448faf
rexim <reximkut@gmail.com>
2019-01-03 01:03:03 +0700

    (#3) Add dir local vars for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((haskell-mode . ((haskell-process-args-cabal-repl . ("--ghc-option=-ferror-spans"
+                                                      "--ghc-option=-Wno-missing-home-modules")))))
